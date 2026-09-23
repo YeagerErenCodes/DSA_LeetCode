bff5da40a9df8b040e3f3642d08a68e6dbe4d8bf
YeagerEren <214592036+YeagerErenCodes@users.noreply.github.com>
2026-09-23 18:54:17 +0530

    Time: 4 ms (1.19%), Space: 12.7 MB (12.51%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -4,12 +4,10 @@ public:
         vector<int > ans(nums.size());
         for(int i=0;i<nums.size();i++){
             int currentSum=0;
-            for(int j=0;j<=i;j++){
+            for(int j=0;j<=i;++j){
                 currentSum+=nums[j];
             }
             ans[i]=currentSum;
-
-
         }
         return ans;
         
